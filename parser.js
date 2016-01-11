f2d4686b241e3a7780a3c6b5c781ff13cb4300cb
nyxsys1 <mbarbera@udel.edu>
2016-01-11 23:04:13 +0000

    added regex to parser

--- parser.js
+++ parser.js
@@ -1,19 +1,81 @@
 function parseInput(text){
     
-    /*
-    var myRegs = [/look (.*)/g,/move (.*)/g];
-    var findKey = true;
+   
+    var reg_move = /move (.*)/i;
+    var reg_go = /go (.*)/i;
+    var reg_talk = /talk (.*)/i;
+    var reg_examine = /examine (.*)/i;
+    var reg_take = /take (.*)/i;
+    var reg_look = /look/i;
+    var reg_inventory = /inventory/i;
+    var reg_help = /help/i;
     
-    var myArray = myRe.exec(text);
-    while(findKey){
+
+    
+    if(reg_move.test(text)){
+        var match = reg_move.exec(text);
+        move(match[1]);
         
-        
-        if(myArray[1]){
-            
-        }    
+        return;
     }
-    */
     
+    if(reg_go.test(text)){
+        var match = reg_go.exec(text);
+        move(match[1]);
+        
+        return;
+    }
+    
+    if(reg_talk.test(text)){
+        var match = reg_talk.exec(text);
+        talk(match[1]);
+        
+        return;
+    }
+    
+
+    
+    if(reg_examine.test(text)){
+        var match = reg_examine.exec(text);
+        examine(match[1]);
+        
+        return;
+    }
+    
+    if(reg_take.test(text)){
+        var match = reg_take.exec(text);
+        take(match[1]);
+        
+        return;
+    }
+    
+    if(reg_look.test(text)){
+        look();
+        
+        return;
+    }
+    
+    if(reg_inventory.test(text)){
+        inventory();
+        
+        
+        return;
+    }
+    if(reg_help.test(text)){
+        help();
+        
+        return;
+    }
+    
+    /*global output*/
+     output("Invalid command {0}! (Type help to see commands)".format(text));    
+    
+    
+    
+        
+      
+    
+    /*
     var split = text.split(" ");
     if(split.length > 0 && split[0] === "inventory"){
         inventory();
@@ -27,6 +89,7 @@ function parseInput(text){
         move(split[1]);
         return;
     }
+    */
     
-    output("Invalid command {0}! (Type help to see commands)".format(myArray));
+   
 }
