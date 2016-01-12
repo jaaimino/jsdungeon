cf6fe15b04f24f5c3c2d4856af143624fc42ed7d
nyxsys1 <mbarbera@udel.edu>
2016-01-12 03:09:54 +0000

    fixed whitespace regex

--- parser.js
+++ parser.js
@@ -3,12 +3,12 @@ function parseInput(text){
    
     var reg_move = /(?:move|go|skedaddle) (.*)/i;
     var reg_talk = /(?:talk|chat) (.*)/i;
-    var reg_examine = /examine (.*)/i;
+    var reg_examine = /(?:examine|check|scrutinize) (.*)/i;
     var reg_take = /(?:take|grab|snatch) (.*)/i;
-    var reg_look = /look/i;
+    var reg_look = /(?:look|review|survey)/i;
     var reg_inventory = /inventory/i;
     var reg_help = /help/i;
-    var whitespace = /.*\\w.*/;
+    var whitespace = /\S/;
     
 
     
@@ -69,29 +69,10 @@ function parseInput(text){
     }
     
     
+    
     /*global output*/
      output("Invalid command {0}! (Type help to see commands)".format(text));    
     
-    
-    
-        
-      
-    
-    /*
-    var split = text.split(" ");
-    if(split.length > 0 && split[0] === "inventory"){
-        inventory();
-        return;
-    }
-    if(split.length > 0 && split[0] === "look"){
-        look();
-        return;
-    }
-    if(split.length > 0 && split[0] === "go" && split[1]){
-        move(split[1]);
-        return;
-    }
-    */
     
    
 }
