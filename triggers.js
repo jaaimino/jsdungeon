1fc334aedc73395f9bedb683ba0f21122ad83226
jaaimino <jacobaimino@gmail.com>
2016-01-13 15:33:15 +0000

    Added state change trigger

--- triggers.js
+++ triggers.js
@@ -1,9 +1,12 @@
 /* File for handling triggers (This is going to get messy) */
 function process_trigger(trigger){
-    if(trigger.type === "change_state"){
-        
+    if(trigger.type === "state_change"){
+        trigger_change_state(trigger);
     }
 }
 
 function trigger_change_state(trigger){
+    currentDungeon[trigger.target_type][trigger.target].current_state = trigger.new_state;
+    output(trigger.description);
+    //console.log(currentDungeon);
 }
