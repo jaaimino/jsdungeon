b1a81fe1ee44d137482ae36b702bc374669fa811
jaaimino <jacobaimino@gmail.com>
2016-01-15 15:52:29 +0000

    Game saves now

--- savegame.js
+++ savegame.js
@@ -1,8 +1,26 @@
 /* Will use localstorage to save game */
 
-function localStorageTest(){
+function localStorageTest(currentDungeon){
     // Store
     localStorage.setItem("lastname", "Smith");
     // Retrieve
     localStorage.getItem("lastname");
+}
+
+function saveGame(dungeonName, currentDungeon){
+    localStorage.setItem(dungeonName, JSON.stringify(currentDungeon));
+}
+
+/*
+ * Returns localstorage object save or null
+ */
+function loadGame(dungeonName){
+    try{
+        var dungeon = JSON.parse(localStorage.getItem(dungeonName));
+        return dungeon;
+    }catch(err){
+        console.log(err);
+        return null;
+    }
+    return null;
 }
