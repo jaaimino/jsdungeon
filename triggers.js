7af981e5cfd4a8b6fa2a077d91b0544dbb43e9f0
jaaimino <jacobaimino@gmail.com>
2016-01-17 18:55:40 +0000

    Updated triggers to not require currentDungeon or currentArea.

--- triggers.js
+++ triggers.js
@@ -1,7 +1,7 @@
 /* File for handling triggers (This is going to get messy) */
-function process_trigger(currentDungeon, trigger){
+function process_trigger(trigger){
     if(trigger.trigger_type === "state_change"){
-        trigger_change_state(currentDungeon, trigger);
+        trigger_change_state(trigger);
     }
     if(trigger.trigger_type === "lose_game"){
         trigger_lose_game(trigger);
@@ -10,31 +10,52 @@ function process_trigger(currentDungeon, trigger){
         trigger_win_game(trigger);
     }
     if(trigger.trigger_type === "add_item"){
-        trigger_add_item(currentDungeon, trigger);
+        trigger_add_item(trigger);
     }
     if(trigger.trigger_type === "remove_item"){
-        trigger_remove_item(currentDungeon, trigger);
+        trigger_remove_item(trigger);
     }
 }
 
-function trigger_change_state(currentDungeon, trigger){
-    currentDungeon[trigger.target_type][trigger.target].current_state = trigger.new_state;
+function getTargetGroup(trigger){
+    if(trigger.target_type === "objects"){
+        if(trigger.target_room){
+            return getObjects(trigger.target_room);
+        } else {
+            return getObjects();
+        }
+    }
+    if(trigger.target_type === "items"){
+        return getItems();
+    }
+    if(trigger.target_type === "exits"){
+        if(trigger.target_room){
+            return getExits(trigger.target_room);
+        } else {
+            return getExits();
+        }
+    }
+}
+
+function trigger_change_state(trigger){
+    var targetGroup = getTargetGroup(trigger);
+    targetGroup[trigger.target].current_state = trigger.new_state;
     if(trigger.description){
         output(trigger.description);
     }
 }
 
-function trigger_add_item(currentDungeon, trigger){
+function trigger_add_item(trigger){
     var item = trigger.target_item;
-    currentDungeon.player.inventory.push(item);
+    getCurrentDungeon().player.inventory.push(item);
     if(trigger.description){
         output(trigger.description);
     }
 }
 
-function trigger_remove_item(currentDungeon, trigger){
+function trigger_remove_item(trigger){
     var item = trigger.target_item;
-    currentDungeon.player.inventory.remove(item);
+    getCurrentDungeon().player.inventory.remove(item);
     if(trigger.description){
         output(trigger.description);
     }
