af98a3d0e322776e794f739dc8db058a5a56646f
nyxsys <isaprettycoolguy@gmail.com>
2016-05-10 13:19:12 -0400

    added trigger array

--- src/jsdungeon.js
+++ src/jsdungeon.js
@@ -1,5 +1,12 @@
 jsdungeon = {};
-
+jsdungeon.trigger_types = [
+  "state_change",
+  "flavor_text",
+  "lose_game",
+  "win_game",
+  "add_item",
+  "remove_item"
+];
 /* Global variables :( */
 var game_over = false;
 var currentDungeon = null;
